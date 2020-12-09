a43e323596dff78aa5a703ef3b08ff608f12f3e2
Heisenberg0711 <heisenberg950711@gmail.com>
2020-12-09 14:31:00 -0800

    add video function

--- classes/world.js
+++ classes/world.js
@@ -2,6 +2,12 @@
 /* global Avatar */
 /* global BABYLON */
 
+let videoWidth = 1920
+let videoHeight = 1080
+let videoViewLength = 100
+let videoViewHeight = 56.25
+let tvThickness = .2
+
 class World {
     static init() {
         World.canvas = document.getElementById("canvas");
@@ -16,17 +22,45 @@ class World {
         // MY METHODS
         World.addMesh();
 
+        
+        //TV Box
+        var box = BABYLON.MeshBuilder.CreateBox("box", { size: 1 }, World.scene);
+        box.scaling = new BABYLON.Vector3(videoViewLength + tvThickness, videoViewHeight + tvThickness, tvThickness / 2 + .01);
+        box.position = new BABYLON.Vector3(0, 30, tvThickness / 2 + .01);
+
+        // The CSS object will follow this mesh
+        var videoViewMesh = BABYLON.MeshBuilder.CreatePlane("videoViewMesh", { width: 1, height: 1 }, World.scene);
+        videoViewMesh.scaling.x = videoViewLength
+        videoViewMesh.scaling.y = videoViewHeight
+
+        // add actionManager
+        videoViewMesh.actionManager = new BABYLON.ActionManager(World.scene);
+        videoViewMesh.actionManager.registerAction(
+            new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger,
+                function (event) {
+                    var pickedMesh = event.meshUnderPointer;
+                    window.open("https://hiteshsahu.com/FluidSimulation");
+                })
+        );
+
+        //Setup the CSS css3DRenderer and Youtube object
+        let css3DRenderer = setupRenderer(videoViewMesh, 'SCtf4WiXlNc', World.scene, engine);
+
+        css3DRenderer.render(World.scene, World.scene.activeCamera);
         engine.runRenderLoop(() => {
             World.scene.render();
             Avatar.update();
             World.updateCamera();
+            
         });        
         
         //Resize event
         window.addEventListener("resize", () => {
             engine.resize();
         });
+    
     }
+    
     
     static setupCamera() {
         World.camera = new BABYLON.FreeCamera("firstPersonCam", BABYLON.Vector3.Zero(), World.scene);
@@ -165,3 +199,284 @@ class World {
 }
 
 World.cameraDistance = .1;
+
+function removeDomNode(id) {
+    let node = document.getElementById(id);
+    if (node && node.parentNode) {
+        node.parentNode.removeChild(node);
+    }
+}
+
+setupRenderer = (videoViewMesh, videoID, scene, engine) => {
+
+    //div holding WebGL scene canvas
+    let webGLContainer = document.getElementById('canvas')
+
+    //Remove Old Nodes
+    removeDomNode("cssContainer")
+    removeDomNode("CSS3DRendererDom")
+
+    //Add Container for CSS
+    //Canvas Container for css3DRenderer
+    let css3DContainer = document.createElement('div')
+    css3DContainer.id = 'cssContainer'
+    css3DContainer.style.position = 'absolute'
+    css3DContainer.style.width = '100%'
+    css3DContainer.style.height = '100%'
+    css3DContainer.style.zIndex = '-1'
+
+    //add css3DContainer behind WebGl scene
+    webGLContainer.insertBefore(css3DContainer, webGLContainer.firstChild)
+
+    //Add CSS3D css3DRenderer
+    let css3DRenderer = new CSS3DRenderer()
+
+    //appendcss3DRenderer in css3DContainer behind WebGL scene
+    css3DContainer.appendChild(css3DRenderer.domElement)
+
+    //Set CSS container size same as WebGL Container Size
+    css3DRenderer.setSize(webGLContainer.offsetWidth, webGLContainer.offsetHeight)
+
+    //add iframe Container
+    var iframeContainer = document.createElement('div')
+    iframeContainer.style.width = videoWidth + 'px'
+    iframeContainer.style.height = videoHeight + 'px'
+    iframeContainer.style.backgroundColor = '#000'
+    iframeContainer.id = "iframeContainer"
+
+    //CSS Object
+    var CSSobject = new CSS3DObject(iframeContainer, scene)
+    CSSobject.position.copyFrom(videoViewMesh.getAbsolutePosition())
+    CSSobject.rotation.y = -videoViewMesh.rotation.y
+    CSSobject.scaling.copyFrom(videoViewMesh.scaling)
+
+    // append iframe
+    var iframe = document.createElement('iframe')
+    iframe.id = 'video-' + videoID
+    iframe.style.width = videoWidth + 'px'
+    iframe.style.height = videoHeight + 'px'
+    iframe.style.border = '0px'
+    iframe.allow = 'autoplay'
+    iframe.src = ['https://www.youtube.com/embed/', videoID, '?rel=0&enablejsapi=1&disablekb=1&autoplay=1&controls=0&fs=0&modestbranding=1'].join('')
+    iframeContainer.appendChild(iframe)
+
+    //material
+    let depthMask = new BABYLON.StandardMaterial('VideoViewMaterial', scene)
+    depthMask.backFaceCulling = true
+    videoViewMesh.material = depthMask
+
+    //Render Video the mesh
+    videoViewMesh.onBeforeRenderObservable.add(() => engine.setColorWrite(false))
+    videoViewMesh.onAfterRenderObservable.add(() => engine.setColorWrite(true))
+
+    // swap meshes to put mask first
+    var videoPlaneIndex = scene.meshes.indexOf(videoViewMesh)
+    scene.meshes[videoPlaneIndex] = scene.meshes[0]
+    scene.meshes[0] = videoViewMesh
+
+    return css3DRenderer
+}
+class CSS3DObject extends BABYLON.Mesh {
+    constructor(element, scene) {
+        super()
+        this.element = element
+        this.element.style.position = 'absolute'
+        this.element.style.pointerEvents = 'auto'
+    }
+}
+
+class CSS3DRenderer {
+    constructor() {
+        var matrix = new BABYLON.Matrix()
+
+        this.cache = {
+            camera: { fov: 0, style: '' },
+            objects: new WeakMap()
+        }
+
+        var domElement = document.createElement('div')
+        domElement.style.overflow = 'hidden'
+        domElement.id = "CSS3DRendererDom"
+
+        this.domElement = domElement
+        this.cameraElement = document.createElement('div')
+        this.isIE = (!!document['documentMode'] || /Edge/.test(navigator.userAgent) || /Edg/.test(navigator.userAgent))
+
+        if (!this.isIE) {
+            this.cameraElement.style.webkitTransformStyle = 'preserve-3d'
+            this.cameraElement.style.transformStyle = 'preserve-3d'
+        }
+        this.cameraElement.style.pointerEvents = 'none'
+
+        domElement.appendChild(this.cameraElement)
+    }
+
+    getSize = () => {
+        return {
+            width: this.width,
+            height: this.height
+        }
+    }
+
+    setSize = (width, height) => {
+        this.width = width
+        this.height = height
+        this.widthHalf = this.width / 2
+        this.heightHalf = this.height / 2
+
+        this.domElement.style.width = width + 'px'
+        this.domElement.style.height = height + 'px'
+
+        this.cameraElement.style.width = width + 'px'
+        this.cameraElement.style.height = height + 'px'
+    }
+
+    epsilon(value) {
+        return Math.abs(value) < 1e-10 ? 0 : value
+    }
+
+    getCameraCSSMatrix = (matrix) => {
+        var elements = matrix.m
+
+        return 'matrix3d(' +
+            this.epsilon(elements[0]) + ',' +
+            this.epsilon(- elements[1]) + ',' +
+            this.epsilon(elements[2]) + ',' +
+            this.epsilon(elements[3]) + ',' +
+            this.epsilon(elements[4]) + ',' +
+            this.epsilon(- elements[5]) + ',' +
+            this.epsilon(elements[6]) + ',' +
+            this.epsilon(elements[7]) + ',' +
+            this.epsilon(elements[8]) + ',' +
+            this.epsilon(- elements[9]) + ',' +
+            this.epsilon(elements[10]) + ',' +
+            this.epsilon(elements[11]) + ',' +
+            this.epsilon(elements[12]) + ',' +
+            this.epsilon(- elements[13]) + ',' +
+            this.epsilon(elements[14]) + ',' +
+            this.epsilon(elements[15]) +
+            ')'
+    }
+
+    getObjectCSSMatrix = (matrix, cameraCSSMatrix) => {
+        var elements = matrix.m;
+        var matrix3d = 'matrix3d(' +
+            this.epsilon(elements[0]) + ',' +
+            this.epsilon(elements[1]) + ',' +
+            this.epsilon(elements[2]) + ',' +
+            this.epsilon(elements[3]) + ',' +
+            this.epsilon(- elements[4]) + ',' +
+            this.epsilon(- elements[5]) + ',' +
+            this.epsilon(- elements[6]) + ',' +
+            this.epsilon(- elements[7]) + ',' +
+            this.epsilon(elements[8]) + ',' +
+            this.epsilon(elements[9]) + ',' +
+            this.epsilon(elements[10]) + ',' +
+            this.epsilon(elements[11]) + ',' +
+            this.epsilon(elements[12]) + ',' +
+            this.epsilon(elements[13]) + ',' +
+            this.epsilon(elements[14]) + ',' +
+            this.epsilon(elements[15]) +
+            ')'
+
+        if (this.isIE) {
+            return 'translate(-50%,-50%)' +
+                'translate(' + this.widthHalf + 'px,' + this.heightHalf + 'px)' +
+                cameraCSSMatrix +
+                matrix3d;
+        }
+        return 'translate(-50%,-50%)' + matrix3d
+    }
+
+    renderObject = (object, scene, camera, cameraCSSMatrix) => {
+        if (object instanceof CSS3DObject) {
+            var style
+            var objectMatrixWorld = object.getWorldMatrix().clone()
+            var camMatrix = camera.getWorldMatrix()
+            var innerMatrix = objectMatrixWorld.m
+
+            // Set scaling
+            const youtubeVideoWidth = videoWidth / 100
+            const youtubeVideoHeight = videoHeight / 100
+
+            innerMatrix[0] *= 0.01 / youtubeVideoWidth
+            innerMatrix[2] *= 0.01 / youtubeVideoWidth
+            innerMatrix[5] *= 0.01 / youtubeVideoHeight
+
+            // Set position from camera
+            innerMatrix[12] = -camMatrix.m[12] + object.position.x
+            innerMatrix[13] = -camMatrix.m[13] + object.position.y
+            innerMatrix[14] = camMatrix.m[14] - object.position.z
+            innerMatrix[15] = camMatrix.m[15] * 0.00001
+
+            objectMatrixWorld = BABYLON.Matrix.FromArray(innerMatrix)
+            if (this.isIE) {
+                // IE will round numbers like 1e-005 to zero so we need to scale whole matrix up.
+                // Side-effect is reduced accuracy with CSS object mapping to Babylon.js object
+                objectMatrixWorld = objectMatrixWorld.scale(100)
+            }
+            style = this.getObjectCSSMatrix(objectMatrixWorld, cameraCSSMatrix)
+            var element = object.element
+            var cachedObject = this.cache.objects.get(object)
+
+            if (cachedObject === undefined || cachedObject.style !== style) {
+                element.style.webkitTransform = style
+                element.style.transform = style
+                var objectData = { style: style }
+                this.cache.objects.set(object, objectData)
+            }
+            if (element.parentNode !== this.cameraElement) {
+                this.cameraElement.appendChild(element)
+            }
+
+        } else if (object instanceof BABYLON.Scene) {
+            for (var i = 0, l = object.meshes.length; i < l; i++) {
+                this.renderObject(object.meshes[i], scene, camera, cameraCSSMatrix)
+            }
+        }
+    }
+
+    render = (scene, camera) => {
+        var projectionMatrix = camera.getProjectionMatrix()
+        var fov = projectionMatrix.m[5] * this.heightHalf
+
+        if (this.cache.camera.fov !== fov) {
+
+            if (camera.mode == BABYLON.Camera.PERSPECTIVE_CAMERA) {
+                this.domElement.style.webkitPerspective = fov + 'px'
+                this.domElement.style.perspective = fov + 'px'
+            } else {
+                this.domElement.style.webkitPerspective = ''
+                this.domElement.style.perspective = ''
+            }
+            this.cache.camera.fov = fov
+        }
+
+        if (camera.parent === null) camera.computeWorldMatrix()
+
+        var matrixWorld = camera.getWorldMatrix().clone()
+        var rotation = matrixWorld.clone().getRotationMatrix().transpose()
+        var innerMatrix = matrixWorld.m
+
+        innerMatrix[1] = rotation.m[1]
+        innerMatrix[2] = -rotation.m[2]
+        innerMatrix[4] = -rotation.m[4]
+        innerMatrix[6] = -rotation.m[6]
+        innerMatrix[8] = -rotation.m[8]
+        innerMatrix[9] = -rotation.m[9]
+
+        matrixWorld = BABYLON.Matrix.FromArray(innerMatrix)
+
+        var cameraCSSMatrix = 'translateZ(' + fov + 'px)' + this.getCameraCSSMatrix(matrixWorld)
+
+        var style = cameraCSSMatrix + 'translate(' + this.widthHalf + 'px,' + this.heightHalf + 'px)'
+
+        if (this.cache.camera.style !== style && !this.isIE) {
+            this.cameraElement.style.webkitTransform = style
+            this.cameraElement.style.transform = style
+            this.cache.camera.style = style
+        }
+
+        this.renderObject(scene, scene, camera, cameraCSSMatrix)
+    }
+}
