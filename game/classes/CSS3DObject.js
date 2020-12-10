99793b14df7792c80fb17767aa7d2353b1ae4145
Caiting Wu <mrctwu@gmail.com>
2020-12-09 17:47:15 -0800

    Added basic files for video streaming

---
+++ game/classes/CSS3DObject.js
@@ -0,0 +1,8 @@
+class CSS3DObject extends BABYLON.Mesh {
+    constructor(element) {
+        super();
+        this.element = element;
+        this.element.style.position = "absolute";
+        this.element.style.pointerEvents = "auto";
+    }
+}
